3e46e72002858c54420dfa44a105c674d5702830
Yu-Jun Sun <91317039+SunYujun0725@users.noreply.github.com>
2024-05-29 04:06:32 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+/*
+ * @Author: SunYujun0725 91317039+SunYujun0725@users.noreply.github.com
+ * @Date: 2024-05-29 04:04:00
+ * @LastEditors: SunYujun0725 91317039+SunYujun0725@users.noreply.github.com
+ * @LastEditTime: 2024-05-29 04:04:09
+ * @FilePath: /html/script.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+let display = document.getElementById('display');
+let currentOperand = '';
+let previousOperand = '';
+let operation = null;
+
+function appendNumber(number) {
+    if (number === '.' && currentOperand.includes('.')) return;
+    currentOperand = currentOperand.toString() + number.toString();
+    updateDisplay();
+}
+
+function chooseOperation(op) {
+    if (currentOperand === '') return;
+    if (previousOperand !== '') {
+        calculate();
+    }
+    operation = op;
+    previousOperand = currentOperand;
+    currentOperand = '';
+}
+
+function calculate() {
+    let computation;
+    const prev = parseFloat(previousOperand);
+    const current = parseFloat(currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (operation) {
+        case '+':
+            computation = prev + current;
+            break;
+        case '-':
+            computation = prev - current;
+            break;
+        case '*':
+            computation = prev * current;
+            break;
+        case '/':
+            computation = prev / current;
+            break;
+        default:
+            return;
+    }
+    currentOperand = computation;
+    operation = null;
+    previousOperand = '';
+    updateDisplay();
+}
+
+function clearDisplay() {
+    currentOperand = '';
+    previousOperand = '';
+    operation = null;
+    updateDisplay();
+}
+
+function updateDisplay() {
+    display.value = currentOperand;
+}
